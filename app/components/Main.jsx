616189f71636978c26c1484e0fb06ec8ca1d6ca5
Jackson Tong <tongtoan2704@gmail.com>
2016-08-04 23:08:18 +0700

    navigation styles

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,16 +1,18 @@
-var React = require('react');
+import React from 'react';
+import Nav from './Nav';
 
-var Main = (props) => {
+const Main = (props) => {
   return (
     <div>
       <div>
         <div>
+          <Nav/>
           <p>Main.jsx Rendered</p>
           {props.children}
         </div>
       </div>
     </div>
   );
-}
+};
 
 module.exports = Main;
