dcef7ec3023b3d46df288c673b180d1ec383e120
Jackson Tong <tongtoan2704@gmail.com>
2016-08-06 00:09:42 +0700

    update jQuery

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import expect from 'expect';
-import $ from 'jQuery';
+var $ = require('jQuery');
 import TestUtils from 'react-addons-test-utils';
 import Clock from '../../components/Clock';
 
