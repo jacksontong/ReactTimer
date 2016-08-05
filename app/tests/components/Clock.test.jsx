ae27e8ea4481aefd9e9ba5958849956c6b81d1d9
Jackson Tong <tongtoan2704@gmail.com>
2016-08-06 00:19:37 +0700

    import jquery

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import expect from 'expect';
-var $ = require('jQuery');
+import $ from 'jquery/dist/jquery';
 import TestUtils from 'react-addons-test-utils';
 import Clock from '../../components/Clock';
 
